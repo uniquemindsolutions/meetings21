33ee53c1b8aa508960319e67cd3972c5c13c56a3
Upenderm8030 <167904523+Upenderm8030@users.noreply.github.com>
2025-01-21 23:38:41 +0530

    speakers and venue added api
    speakers and venue added api

--- src/Screens/Speakers.jsx
+++ src/Screens/Speakers.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from 'react'
 import Footer from '../Footer/footer'
 import EventHeader from '../Header/EventHeader'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 const Speakers = () => {
 
-  const spekersUrl = process.env.REACT_APP_API_URL;
+  const committeeUrl = process.env.REACT_APP_API_URL;
+  const location = useLocation();
 
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const [speakerData, setSpekerData] = useState([])
+  const [currentEventName, setCurrentEventName] = useState('');
+  const [currentEventId, setCurrentEventId] = useState('');
+  const [speakerData, setSpeakerData] = useState('');
   const [openModalId, setOpenModalId] = useState(null);
 
   useEffect(()=>{
@@ -16,72 +23,74 @@ const Speakers = () => {
   }, [])
 
   const getSpekers = async ()=>{
-    setLoading(true);
-    try {
-      const res = await axios.get(`${spekersUrl}speakers/`);
-      setSpekerData(res.data)
-      console.log("speakers data ===", res)
-    }catch {
-      setError('No data found')
-    }
+    const currentEvents = location.pathname.split('/');
+  setCurrentEventName(currentEvents[1])
+  console.log("currentEventNameres ===", currentEvents[1], currentEvents)
+  try {
+      const res = await  axios.get(`${committeeUrl}${currentEvents[1]}/agenda/speakers/`);
+      setSpeakerData(res.data);
+      console.log('speakersdata ===', res.data);
+  } catch {
+      setError('Error loaded')
+  }
   }
 
   const openModal = (id) => setOpenModalId(id);
   const closeModal = () => setOpenModalId(null);
-  const speakers = [
-    {
-        id: 1,
-        name: "Federico Rosei",
-        talkTitle: "Lorem Ipsum is simply dummy text",
-        Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
-        image: "images/speakers/pspeaker1.png",
-        details: [
-            "2010 PEO Engineering Medal for Entrepreneurship",
-            "2013 EIC Sir John Kennedy Medal",
-            "2016 IEEE A.G.L. McNaughton Gold Medal",
-        ],
-        position: "INRS, Canada",
-    },
-    {
-        id: 2,
-        name: "Ben Zhong TANG",
-        talkTitle: "Lorem Ipsum is simply dummy text",
-        Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
-        image: "images/speakers/pspeaker2.jpg",
-        details: [
-            "2010 PEO Engineering Medal for Entrepreneurship",
-            "2013 EIC Sir John Kennedy Medal",
-            "2016 IEEE A.G.L. McNaughton Gold Medal",
-        ],
-        position: "Ben Zhong TANG",
-    },
-    {
-        id: 3,
-        name: "Jose M. Kenny",
-        talkTitle: "Lorem Ipsum is simply dummy text",
-        Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
-        image: "images/speakers/pspeaker3.jpg",
-        details: [
-            "2010 PEO Engineering Medal for Entrepreneurship",
-            "2013 EIC Sir John Kennedy Medal",
-            "2016 IEEE A.G.L. McNaughton Gold Medal",
-        ],
-        position: "University of Perugia, Italy",
-    },
-    {
-        id: 4,
-        name: "Jan DUSZA",
-        talkTitle: "Lorem Ipsum is simply dummy text",
-        Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
-        image: "images/speakers/pspeaker4.jpg",
-        details: [
-            "2010 PEO Engineering Medal for Entrepreneurship",
-            "2013 EIC Sir John Kennedy Medal",
-            "2016 IEEE A.G.L. McNaughton Gold Medal",
-        ],
-        position: "Institute of Materials Research of SAS, Slovakia",
-    },
-];
+//   const speakers = [
+//     {
+//         id: 1,
+//         name: "Federico Rosei",
+//         talkTitle: "Lorem Ipsum is simply dummy text",
+//         Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
+//         image: "images/speakers/pspeaker1.png",
+//         details: [
+//             "2010 PEO Engineering Medal for Entrepreneurship",
+//             "2013 EIC Sir John Kennedy Medal",
+//             "2016 IEEE A.G.L. McNaughton Gold Medal",
+//         ],
+//         position: "INRS, Canada",
+//     },
+//     {
+//         id: 2,
+//         name: "Ben Zhong TANG",
+//         talkTitle: "Lorem Ipsum is simply dummy text",
+//         Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
+//         image: "images/speakers/pspeaker2.jpg",
+//         details: [
+//             "2010 PEO Engineering Medal for Entrepreneurship",
+//             "2013 EIC Sir John Kennedy Medal",
+//             "2016 IEEE A.G.L. McNaughton Gold Medal",
+//         ],
+//         position: "Ben Zhong TANG",
+//     },
+//     {
+//         id: 3,
+//         name: "Jose M. Kenny",
+//         talkTitle: "Lorem Ipsum is simply dummy text",
+//         Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
+//         image: "images/speakers/pspeaker3.jpg",
+//         details: [
+//             "2010 PEO Engineering Medal for Entrepreneurship",
+//             "2013 EIC Sir John Kennedy Medal",
+//             "2016 IEEE A.G.L. McNaughton Gold Medal",
+//         ],
+//         position: "University of Perugia, Italy",
+//     },
+//     {
+//         id: 4,
+//         name: "Jan DUSZA",
+//         talkTitle: "Lorem Ipsum is simply dummy text",
+//         Biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the",
+//         image: "images/speakers/pspeaker4.jpg",
+//         details: [
+//             "2010 PEO Engineering Medal for Entrepreneurship",
+//             "2013 EIC Sir John Kennedy Medal",
+//             "2016 IEEE A.G.L. McNaughton Gold Medal",
+//         ],
+//         position: "Institute of Materials Research of SAS, Slovakia",
+//     },
+// ];
 
 
   return (
@@ -109,18 +118,20 @@ const Speakers = () => {
                 Our Plenary and Keynote Speakers possess specialized knowledge and insights into specific fields. They can provide valuable information and trends, making them of great benefit to our attendees.
               </p>
             </div>
+            { Array.isArray(speakerData) &&
+                                speakerData.length > 0 ? (speakerData.map((item, index) => (
             <div className='row'>
-              <div className="col-lg-12 mb-2"><h2>Plenary Speakers</h2></div>
-              {speakers.map((speaker) => (
+              <div className="col-lg-12 mb-2"><h2>{speakerData.speakers_type}</h2></div>
+            
                 <div
-                  key={speaker.id}
+                  key={item.id}
                   className="col-md-3 col-sm-12 col-xs-12 team-block text-left margin-30px-bottom team-style-1 sm-margin-seven-bottom xs-margin-40px-bottom wow fadeInRight"
                 >
                   <figure className="speakers-box">
                     <div className="team-image xs-width-100">
                       <img
-                        src={speaker.image}
-                        alt={speaker.name}
+                        src={item.speakerphoto}
+                        alt={item.speakername}
                         data-no-retina=""
                       />
                       <div className="overlay-content text-center">
@@ -128,25 +139,24 @@ const Speakers = () => {
                           <div className="vertical-align-bottom display-table-cell icon-social-small padding-twelve-all">
                             <div className="min-height-176px">
                               <ul className="text-white font-size-15px display-inline-block no-margin text-left">
-                                {speaker.details.map((detail, index) => (
-                                  <li key={index}>
-                                     {detail}</li>
-                                )
-                                )}
+                              {item.speaker_biography}
                               </ul>
                             </div>
-                            <div className="separator-line-horrizontal-full bg-light-blue margin-eleven-tb bg-white"></div>
                             <a
                               href="#"
                               className="text-white font-size-24px"
                               onClick={(e) => {
                                 e.preventDefault();
-                                openModal(speaker.id);
+                                openModal(item.id);
                               }}
                             >
                               {/* <i className="fa-regular fa-eye"></i> */}
                               <button className='btn-biography'> View Biography </button>
                             </a>
+                            {/* <button>sddfsdfsdfsd</button> */}
+                            <div className="separator-line-horrizontal-full bg-light-blue margin-eleven-tb bg-white"></div>
+                           
+                           
                           </div>
                         </div>
                       </div>
@@ -155,153 +165,40 @@ const Speakers = () => {
                     <figcaption>
                       <div className="team-member-position text-center">
                         <div className="text-extra-large font-weight-600 text-extra-dark-gray text-uppercase font-weight-bold">
-                          {speaker.name}
+                          {item.speakername}
                         </div>
                         <div className="text-medium font-weight-500 text-dark-gray font-size-15px">
-                          {speaker.position}
+                          {item.speaker_affiliation}
                         </div>
                       </div>
                     </figcaption>
                   </figure>
                 </div>
-              ))}
+            
             </div>
             
+          ))) : "no data"
+        }
+       
 
-            <div className='row'>
-              <div className="col-lg-12 mb-2"><h2>Keynote Speakers</h2></div>
-              {speakers.map((speaker) => (
-                <div
-                  key={speaker.id}
-                  className="col-md-3 col-sm-12 col-xs-12 team-block text-left margin-30px-bottom team-style-1 sm-margin-seven-bottom xs-margin-40px-bottom wow fadeInRight"
-                >
-                  <figure className="speakers-box">
-                    <div className="team-image xs-width-100">
-                      <img
-                        src={speaker.image}
-                        alt={speaker.name}
-                        data-no-retina=""
-                      />
-                      <div className="overlay-content text-center">
-                        <div className="display-table height-100 width-100">
-                          <div className="vertical-align-bottom display-table-cell icon-social-small padding-twelve-all">
-                            <div className="min-height-176px">
-                              <ul className="text-white font-size-15px display-inline-block no-margin text-left">
-                                {speaker.details.map((detail, index) => (
-                                  <li key={index}>
-                                    {detail}</li>
-                                )
-                                )}
-                              </ul>
-                            </div>
-                            <div className="separator-line-horrizontal-full bg-light-blue margin-eleven-tb bg-white"></div>
-                            <a
-                              href="#"
-                              className="text-white font-size-24px"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                openModal(speaker.id);
-                              }}
-                            >
-                              {/* <i className="fa-regular fa-eye"></i> */}
-                              <button className='btn-biography'> View Biography </button>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="team-overlay bg-extra-dark-gray opacity8"></div>
-                    </div>
-                    <figcaption>
-                      <div className="team-member-position text-center">
-                        <div className="text-extra-large font-weight-600 text-extra-dark-gray text-uppercase font-weight-bold">
-                          {speaker.name}
-                        </div>
-                        <div className="text-medium font-weight-500 text-dark-gray font-size-15px">
-                          {speaker.position}
-                        </div>
-                      </div>
-                    </figcaption>
-                  </figure>
-                </div>
-              ))}
-            </div>
-
-            <div className='row'>
-              <div className="col-lg-12 mb-2"><h2>Invited Speakers</h2></div>
-              {speakers.map((speaker) => (
-                <div
-                  key={speaker.id}
-                  className="col-md-3 col-sm-12 col-xs-12 team-block text-left margin-30px-bottom team-style-1 sm-margin-seven-bottom xs-margin-40px-bottom wow fadeInRight"
-                >
-                  <figure className="speakers-box">
-                    <div className="team-image xs-width-100">
-                      <img
-                        src={speaker.image}
-                        alt={speaker.name}
-                        data-no-retina=""
-                      />
-                      <div className="overlay-content text-center">
-                        <div className="display-table height-100 width-100">
-                          <div className="vertical-align-bottom display-table-cell icon-social-small padding-twelve-all">
-                            <div className="min-height-176px">
-                              <ul className="text-white font-size-15px display-inline-block no-margin text-left">
-                                {speaker.details.map((detail, index) => (
-                                  <li key={index}>
-                                     {detail}</li>
-                                )
-                                )}
-                              </ul>
-                            </div>
-                            <div className="separator-line-horrizontal-full bg-light-blue margin-eleven-tb bg-white"></div>
-                            <a
-                              href="#"
-                              className="text-white font-size-24px"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                openModal(speaker.id);
-                              }}
-                            >
-                              {/* <i className="fa-regular fa-eye"></i> */}
-                              <button className='btn-biography'> View Biography </button>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="team-overlay bg-extra-dark-gray opacity8"></div>
-                    </div>
-                    <figcaption>
-                      <div className="team-member-position text-center">
-                        <div className="text-extra-large font-weight-600 text-extra-dark-gray text-uppercase font-weight-bold">
-                          {speaker.name}
-                        </div>
-                        <div className="text-medium font-weight-500 text-dark-gray font-size-15px">
-                          {speaker.position}
-                        </div>
-                      </div>
-                    </figcaption>
-                  </figure>
-                </div>
-              ))}
-            </div>
+         
           </div>
-          {/* Modal */}
-          {speakers.map(
+
+          {speakerData.map(
                         (speaker) =>
                             openModalId === speaker.id && (
                                 <div key={speaker.id} className="modal-overlay  text-left">
                                     <div className="modal-content text-left">
-                                        {/* <h5 className="modal-title">{speaker.name}</h5> */}
                                         <h5 className='mb-4'>Talk Title: <span style={{ fontWeight: 400 }}>{speaker.talkTitle}</span></h5>
                                         <h5>Biography: </h5>
-                                        <p>{speaker.Biography}</p>
-                                        <p>{speaker.Biography}</p>
+                                        <p>{speaker.speaker_biography}</p>
+                                        <p>{speaker.speaker_biography}</p>
 
-                                        {/* <p>{speaker.position}</p> */}
-                                        {/* <ul>
-                                            {speaker.details.map((detail, index) => (
+                                        <ul>
+                                            {/* {speaker.details.map((detail, index) => (
                                                 <li key={index}>{detail}</li>
-                                            ))}
-                                        </ul> */}
+                                            ))} */}
+                                        </ul>
                                         <div className="text-center mx-auto">
                                             <button
                                                 className="custom-button mt-5"
